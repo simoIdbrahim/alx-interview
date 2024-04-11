513015befc2db158724b593cc22a2cb28dc657f0
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-04-11 07:01:40 -0700

    add

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+Create a function def pascal_triangle(n): that returns a list of lists of
+integers representing the Pascal’s triangle of n:
+"""
 def pascal_triangle(n):
     """
     Generates a Pascal's triangle of size n.
