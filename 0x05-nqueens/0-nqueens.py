7090fd655a9d8e75a4cef9a0a5328c7123e44fb0
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-05-15 07:21:44 -0700

    fix err

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,109 +1,56 @@
 #!/usr/bin/python3
-""" task queens """
+""" task N queens """
 
 
-def queens(chessBoard, r, maje, res):
-    """ queens method """
-    if maje == len(chessBoard):
-        res.append(extract(chessBoard))
-        return (res)
+def solve_n_queens(num):
+    """ solve n queens function """
+    def is_safe(board, row, col):
+        """ is safe function """
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
 
-    for col in range(len(chessBoard)):
-        if chessBoard[r][col] == -1:
-            demo = copy_board(chessBoard)
-            demo[r][col] = 1
-            exit_queens(demo, r, col)
-            res = queens(demo, r + 1, maje + 1, res)
-    return (res)
+    def backtrack(board, row):
+        """ backtrack function """
+        if row == num:
+            solutions.append(list(board))
+            return
 
+        for col in range(num):
+            if is_safe(board, row, col):
+                board[row] = col
+                backtrack(board, row + 1)
 
-def exit_queens(chessBoard, r, col):
-    """ exit_queens method """
-    len = len(chessBoard)
-    for c in range(col + 1, len):
-        chessBoard[r][c] = 0
-    for c in range(col - 1, -1, -1):
-        chessBoard[r][c] = 0
-    for r in range(r + 1, len):
-        chessBoard[r][col] = 0
-    for r in range(r - 1, -1, 1):
-        chessBoard[r][col] = 0
-    c = col + 1
-    for r in range(r + 1, len):
-        if c >= len:
-            break
-        chessBoard[r][c] = 0
-        c += 1
-    c = col - 1
-    for r in range(r - 1, -1, -1):
-        if c < 0:
-            break
-        chessBoard[r][c] = 0
-        c -= 1
-    c = col + 1
-    for r in range(r - 1, -1, -1):
-        if c >= len:
-            break
-        chessBoard[r][c] = 0
-        c += 1
-    c = col - 1
-    for r in range(r + 1, len):
-        if c < 0:
-            break
-        chessBoard[r][c] = 0
-        c -= 1
+    solutions = []
+    chessboard = [-1] * num
+    backtrack(chessboard, 0)
+    return solutions
 
 
-def chessBoard(N):
-    """ chessBoard method"""
-    chessBoard = []
-
-    for r in range(N):
-        chessBoard.append([])
-
-    for r in chessBoard:
-        for n in range(N):
-            r.append(-1)
-
-    return (chessBoard)
+def print_solutions(solutions):
+    """ print solutions function """
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(len(solution))])
 
 
-def copy_board(chessBoard):
-    """ copy_board method """
-    if type(chessBoard) == list:
-        return list(map(copy_board, chessBoard))
-    return (chessBoard)
-
-
-def extract(chessBoard):
-    """ extract method """
-    outcome = []
-    for row in range(len(chessBoard)):
-        for col in range(len(chessBoard)):
-            if chessBoard[row][col] == 1:
-                outcome.append([row, col])
-                break
-    return outcome
-
-
-def execute():
+if __name__ == '__main__':
     import sys
 
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isnumeric() is False:
+    if not sys.argv[1].isdigit():
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
+
+    N = int(sys.argv[1])
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    chess = chessBoard(int(sys.argv[1]))
-    resultMatrix = queens(chess, 0, 0, [])
-    for r in resultMatrix:
-        print(r)
+    solutions = solve_n_queens(N)
+    print_solutions(solutions)
 
-
-if __name__ == '__main__':
-    execute()
