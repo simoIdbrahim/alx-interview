e4c4e59a027dcc35d2206eb0c5d63622317ce77a
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-13 06:32:16 -0700

    fix length

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,17 +4,17 @@
 
 def island_perimeter(grid):
     """
-      returns the perimeter of the island described in grid
+    returns the perimeter of the island described in grid
     """
-    len = len(grid)
+    length = len(grid)
     bre = len(grid[0])
     perimeter = 0
-    for i in range(len):
+    for i in range(length):
         for j in range(bre):
             if grid[i][j] == 1:
                 perimeter += 4
                 if (j < bre - 1 and grid[i][j + 1] == 1):
                     perimeter -= 2
-                if (i < len - 1 and grid[i + 1][j] == 1):
+                if (i < length - 1 and grid[i + 1][j] == 1):
                     perimeter -= 2
     return perimeter
